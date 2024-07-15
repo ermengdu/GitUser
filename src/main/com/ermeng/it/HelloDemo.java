3e42d4431aa42e6b5cd39cbc512e1c8aba4b9861
duermeng <2669498105@qq.com>
2024-07-15 23:09:47 +0800

    首次提交

---
+++ src/main/com/ermeng/it/HelloDemo.java
@@ -0,0 +1,13 @@
+package main.com.ermeng.it;
+
+/**
+ * @Author: Emmett
+ * @Email：2669498105@qq.com
+ * @Date: 2024/7/15 23:01
+ */
+
+public class HelloDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
